0620bbcf51d75be73bbe61701b8e6003fe2b0f26
isiy0 <rodrigomesquita.engsoftware@gmail.com>
2026-05-26 20:46:57 -0300

    feat: add home, contato, and sobre views and configure their corresponding URL routes

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from recipes.views import view_home, view_contato, view_sobre
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', view_home),
+    path('contato/', view_contato),
+    path('sobre/', view_sobre)
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def view_home(request):
+    return HttpResponse("Hello World")
+
+def view_contato(request):
+    return HttpResponse("Página de contato")
+
+def view_sobre(request):
+    return HttpResponse("Página sobre")
