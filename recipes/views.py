db095bf89cfaa8e8e0f7e4c63a414cca19ac0207
isiy0 <rodrigomesquita.engsoftware@gmail.com>
2026-05-26 21:27:47 -0300

    feat: initialize Django project structure with recipes app and base views with namespaces

--- recipes/views.py
+++ recipes/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def view_home(request):
-    return render(request, "home.html")
+    return render(request, "recipes/home.html")
+
 
 def view_contato(request):
     return render(request, "contato.html")
+
 
 def view_sobre(request):
     return render(request, "sobre.html")
